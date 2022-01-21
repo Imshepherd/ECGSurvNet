aaf6b385bf40309adbbcde261245d26bac14db4a
Imshepherd <chaos53438@livemail.tw>
2022-01-21 17:48:56 +0800

    Modify 1. download Sami-Trop.R.

--- code/1. processing data/1. download Sami-Trop.R
+++ code/1. processing data/1. download Sami-Trop.R
@@ -9,7 +9,7 @@ ecg_data_url <- "https://zenodo.org/record/4905618/files/exams.zip?download=1"
 basic_infor_file_path <- "data/raw_data/Sami-Trop.csv"
 
 ecg_data_zip_path <- "data/raw_data/Sami-Trop.zip"
-ecg_data_file_dir <- "data/raw_data/"
+ecg_data_file_dir <- "data/raw_data"
 
 # Download and zip
 
@@ -17,7 +17,7 @@ download.file(url = basic_infor_url, destfile = basic_infor_file_path)
 
 download.file(url = ecg_data_url, destfile = ecg_data_zip_path, mode = "wb")
 unzip(zipfile = ecg_data_zip_path, exdir = ecg_data_file_dir)
-file.rename(from = paste0(ecg_data_file_dir, "exams.hdf5"), to = paste0(ecg_data_file_dir, "Sami-Trop.hdf5"))
+file.rename(from = paste0(ecg_data_file_dir, "/exams.hdf5"), to = paste0(ecg_data_file_dir, "/Sami-Trop.hdf5"))
 
 
 
